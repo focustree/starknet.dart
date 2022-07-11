b98e2e42211ee06f9a7bfad719d34f7437cf11e9
Gabin Marignier <gabin.contact@gmail.com>
2022-07-11 20:03:40 +0200

    Document provider

--- lib/src/provider/provider.dart
+++ lib/src/provider/provider.dart
@@ -9,6 +9,11 @@ abstract class Provider {
   String get gatewayURL;
   String get chainId;
 
+  /// Gets the block information
+  ///
+  /// [Reference](https://github.com/starkware-libs/cairo-lang/blob/167b28bcd940fd25ea3816204fa882a0b0a49603/src/starkware/starknet/services/api/feeder_gateway/feeder_gateway_client.py#L61-L73)
+  ///
+  /// Returns a [GetBlockResponse] class with the block information.
   Future<GetBlockResponse> getBlock();
 }
 
--- lib/starknet.dart
+++ lib/starknet.dart
@@ -1,3 +1,6 @@
+/// The Starknet Dart SDK ✨
+///
+/// It aims at supporting the same API as the official Starknet API, but in Dart.
 library starknet;
 
 export 'src/provider/provider.dart';
