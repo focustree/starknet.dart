2cc6790829c98fcbecb168d2d221acb859415194
Gabin Marignier <gabin.contact@gmail.com>
2022-07-11 19:30:35 +0200

    Improve example to demonstrate get_block usage

--- example/starknet_example.dart
+++ example/starknet_example.dart
@@ -1,3 +1,20 @@
-void main() {
-  print('Looks good!');
+import 'dart:convert';
+
+import 'package:starknet/starknet.dart';
+
+final provider = DefaultProvider(
+  baseURL: 'http://127.0.0.1:5050',
+  feederGatewayURL: 'http://127.0.0.1:5050/feeder_gateway',
+  gatewayURL: 'http://127.0.0.1:5050/gateway',
+  chainId: '0x534e5f474f45524c49',
+);
+
+void main() async {
+  final block = await provider.getBlock();
+  prettyPrintJson(block.toJson());
+}
+
+prettyPrintJson(Map<String, dynamic> json) {
+  var encoder = JsonEncoder.withIndent("  ");
+  print(encoder.convert(json));
 }
