7059149eef76cc60298776404c005feb958b6d5f
Gabin Marignier <gabin.contact@gmail.com>
2022-07-25 20:10:11 +0200

    Implement computeHashOnElements

--- lib/src/crypto/index.dart
+++ lib/src/crypto/index.dart
@@ -1,3 +1,19 @@
+import 'package:starknet/starknet.dart';
+
 export 'keccak.dart';
 export 'pedersen.dart';
 export 'pedersen_params.dart';
+
+/// Computes a hash chain over the data, in the following order:
+/// h(h(h(h(0, data[0]), data[1]), ...), data[n-1]), n)
+///
+/// The hash is initialized with 0 and ends with the data length appended.
+/// The length is appended in order to avoid collisions of the following kind:
+/// H([x,y,z]) = h(h(x,y),z) = H([w, z]) where w = h(x,y).
+///
+/// Spec: https://docs.starknet.io/docs/Hashing/hash-functions/#array-hashing
+BigInt computeHashOnElements(List<BigInt> elements) {
+  return [BigInt.zero, ...elements, BigInt.from(elements.length)].reduce(
+    (previousValue, currentValue) => pedersenHash(previousValue, currentValue),
+  );
+}
