7a4b5bf016268a77c52796c7d430144e10ace3f6
prajwal27 <prajwalpspbelagavi@gmail.com>
2022-08-18 20:25:25 +0530

    update BlockId model to handle different types.

--- lib/src/provider/model/block_id.dart
+++ lib/src/provider/model/block_id.dart
@@ -1,14 +1,13 @@
-import 'dart:convert';
-
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:starknet/starknet.dart';
 
 part 'block_id.freezed.dart';
 part 'block_id.g.dart';
 
-@freezed
-//@JsonSerializable(converters: [BlockIdConverter()])
+@Freezed(fromJson: true, toJson: false)
 class BlockId with _$BlockId {
+  const BlockId._();
+
   const factory BlockId.blockHash({
     required Felt blockHash,
   }) = BlockIdHash;
@@ -21,39 +20,53 @@ class BlockId with _$BlockId {
 
   factory BlockId.fromJson(Map<String, dynamic> json) =>
       _$BlockIdFromJson(json);
+
+  dynamic toJson() {
+    return map(
+      blockHash: (blockIdHash) {
+        return {'block_hash': blockIdHash.blockHash.toJson()};
+      },
+      blockNumber: (blockIdNumber) {
+        return {'block_number': blockIdNumber.blockNumber};
+      },
+      blockTag: (blockIdTag) {
+        return blockIdTag.blockTag;
+      },
+    );
+  }
 }
 
 // Currently not used.
-class BlockIdConverter implements JsonConverter<BlockId, dynamic> {
-  const BlockIdConverter();
-
-  // BlockId can be constructed from a Map as well as a String.
-  @override
-  BlockId fromJson(dynamic value) {
-    if (value is Map) {
-      Map<String, dynamic> json = value as Map<String, dynamic>;
-
-      // you need to find some condition to know which type it is. e.g. check the presence of some field in the json
-      if (json.containsKey('block_hash')) {
-        return BlockId.blockHash(blockHash: json['block_hash']);
-      } else if (json.containsKey('block_number')) {
-        return BlockId.blockNumber(blockNumber: json['block_number']);
-      }
-    } else if (value is String) {
-      return BlockId.blockTag(blockTag: value);
-    }
-
-    throw Exception('Could not determine the BlockId from given value: $json');
-  }
-
-  @override
-  dynamic toJson(BlockId data) {
-    return data.when<dynamic>(blockHash: (Felt blockHash) {
-      return <String, dynamic>{'block_hash': blockHash};
-    }, blockNumber: (int blockNumber) {
-      return <String, dynamic>{'block_number': blockNumber};
-    }, blockTag: (String blockTag) {
-      return blockTag;
-    });
-  }
-}
+// class BlockIdConverter implements JsonConverter<BlockId, dynamic> {
+//   const BlockIdConverter();
+//
+//   // BlockId can be constructed from a Map as well as a String.
+//   @override
+//   BlockId fromJson(dynamic value) {
+//     if (value is Map) {
+//       Map<String, dynamic> json = value as Map<String, dynamic>;
+//
+//       // you need to find some condition to know which type it is. e.g. check the presence of some field in the json
+//       if (json.containsKey('block_hash')) {
+//         return BlockId.blockHash(blockHash: json['block_hash']);
+//       } else if (json.containsKey('block_number')) {
+//         return BlockId.blockNumber(blockNumber: json['block_number']);
+//       }
+//     } else if (value is String) {
+//       return BlockId.blockTag(blockTag: value);
+//     }
+//
+//     throw Exception('Could not determine the BlockId from given value: $json');
+//   }
+//
+//   @override
+//   dynamic toJson(BlockId data) {
+//     return data.when<dynamic>(blockHash: (Felt blockHash) {
+//       return <String, dynamic>{'block_hash': blockHash};
+//     }, blockNumber: (int blockNumber) {
+//       return <String, dynamic>{'block_number': blockNumber};
+//     }, blockTag: (String blockTag) {
+//       return blockTag;
+//     });
+//   }
+// }
--- lib/src/provider/model/block_id.g.dart
+++ lib/src/provider/model/block_id.g.dart
@@ -12,31 +12,13 @@ _$BlockIdHash _$$BlockIdHashFromJson(Map<String, dynamic> json) =>
       $type: json['runtimeType'] as String?,
     );
 
-Map<String, dynamic> _$$BlockIdHashToJson(_$BlockIdHash instance) =>
-    <String, dynamic>{
-      'block_hash': instance.blockHash.toJson(),
-      'runtimeType': instance.$type,
-    };
-
 _$BlockIdNumber _$$BlockIdNumberFromJson(Map<String, dynamic> json) =>
     _$BlockIdNumber(
       blockNumber: json['block_number'] as int,
       $type: json['runtimeType'] as String?,
     );
 
-Map<String, dynamic> _$$BlockIdNumberToJson(_$BlockIdNumber instance) =>
-    <String, dynamic>{
-      'block_number': instance.blockNumber,
-      'runtimeType': instance.$type,
-    };
-
 _$BlockIdTag _$$BlockIdTagFromJson(Map<String, dynamic> json) => _$BlockIdTag(
       blockTag: json['block_tag'] as String,
       $type: json['runtimeType'] as String?,
     );
-
-Map<String, dynamic> _$$BlockIdTagToJson(_$BlockIdTag instance) =>
-    <String, dynamic>{
-      'block_tag': instance.blockTag,
-      'runtimeType': instance.$type,
-    };
