f8a2e34fa71279c007b6cbf8fda446197c5332ba
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-04 22:53:04 +0100

    reduce code duplication

--- lib/src/account.dart
+++ lib/src/account.dart
@@ -20,82 +20,50 @@ class Account {
     this.supportedTxVersion = AccountSupportedTxVersion.v0,
   });
 
-  Future<InvokeTransactionResponse> execute(
-      {required List<FunctionCall> functionCalls,
-      Felt? maxFee,
-      Felt? nonce}) async {
-    switch (supportedTxVersion) {
-      case AccountSupportedTxVersion.v0:
-        return _executev0(
-          functionCalls: functionCalls,
-          maxFee: maxFee,
-          nonce: nonce,
-        );
-
-      case AccountSupportedTxVersion.v1:
-        return _executev1(
-          functionCalls: functionCalls,
-          maxFee: maxFee,
-          nonce: nonce,
-        );
-    }
-  }
-
-  Future<InvokeTransactionResponse> _executev0(
-      {required List<FunctionCall> functionCalls,
-      Felt? maxFee,
-      Felt? nonce}) async {
+  Future<InvokeTransactionResponse> execute({
+    required List<FunctionCall> functionCalls,
+    Felt? maxFee,
+    Felt? nonce,
+  }) async {
     nonce = nonce ?? Felt.fromInt(0);
     maxFee = maxFee ?? defaultMaxFee;
 
-    final signature = signer.signTransactionsV0(
+    final signature = signer.signTransactions(
         transactions: functionCalls,
         contractAddress: accountAddress,
+        version: supportedTxVersion == AccountSupportedTxVersion.v0 ? 0 : 1,
         chainId: chainId,
         entryPointSelectorName: "__execute__",
         maxFee: maxFee,
         nonce: nonce);
 
-    final calldata =
-        functionCallsToCalldata(functionCalls: functionCalls) + [nonce];
+    switch (supportedTxVersion) {
+      case AccountSupportedTxVersion.v0:
+        final calldata =
+            functionCallsToCalldata(functionCalls: functionCalls) + [nonce];
 
-    return provider.addInvokeTransaction(InvokeTransactionRequest(
-      invokeTransaction: InvokeTransactionV0(
-        contractAddress: accountAddress,
-        entryPointSelector: getSelectorByName('__execute__'),
-        calldata: calldata,
-        maxFee: maxFee,
-        signature: signature,
-      ),
-    ));
-  }
-
-  Future<InvokeTransactionResponse> _executev1(
-      {required List<FunctionCall> functionCalls,
-      Felt? maxFee,
-      Felt? nonce}) async {
-    nonce = nonce ?? Felt.fromInt(0);
-    maxFee = maxFee ?? defaultMaxFee;
-
-    final signature = signer.signTransactionsV1(
-      transactions: functionCalls,
-      senderAddress: accountAddress,
-      chainId: chainId,
-      maxFee: maxFee,
-      nonce: nonce,
-    );
-
-    final calldata = functionCallsToCalldata(functionCalls: functionCalls);
-
-    return provider.addInvokeTransaction(
-      InvokeTransactionRequest(
-        invokeTransaction: InvokeTransactionV1(
-            senderAddress: accountAddress,
+        return provider.addInvokeTransaction(InvokeTransactionRequest(
+          invokeTransaction: InvokeTransactionV0(
+            contractAddress: accountAddress,
+            entryPointSelector: getSelectorByName('__execute__'),
             calldata: calldata,
-            signature: signature,
             maxFee: maxFee,
-            nonce: nonce),
-      ),
-    );
+            signature: signature,
+          ),
+        ));
+      case AccountSupportedTxVersion.v1:
+        final calldata = functionCallsToCalldata(functionCalls: functionCalls);
+
+        return provider.addInvokeTransaction(
+          InvokeTransactionRequest(
+            invokeTransaction: InvokeTransactionV1(
+                senderAddress: accountAddress,
+                calldata: calldata,
+                signature: signature,
+                maxFee: maxFee,
+                nonce: nonce),
+          ),
+        );
+    }
   }
 }
--- lib/src/signer.dart
+++ lib/src/signer.dart
@@ -5,56 +5,33 @@ class Signer {
 
   Signer({required this.privateKey});
 
-  List<Felt> signTransactionsV0({
+  List<Felt> signTransactions({
     required List<FunctionCall> transactions,
     required Felt contractAddress,
     required Felt chainId,
+    required int version,
     Felt? nonce,
     Felt? maxFee,
     String entryPointSelectorName = "__execute__",
   }) {
     nonce = nonce ?? Felt.fromInt(0);
     maxFee = maxFee ?? defaultMaxFee;
-    final calldata =
-        functionCallsToCalldata(functionCalls: transactions) + [nonce];
+    final calldata = version == 0
+        ? functionCallsToCalldata(functionCalls: transactions) + [nonce]
+        : functionCallsToCalldata(functionCalls: transactions);
 
     final transactionHash = calculateTransactionHashCommon(
-        txHashPrefix: TransactionHashPrefix.invoke.toBigInt(),
-        contractAddress: contractAddress.toBigInt(),
-        entryPointSelector:
-            getSelectorByName(entryPointSelectorName).toBigInt(),
-        calldata: toBigIntList(calldata),
-        maxFee: maxFee.toBigInt(),
-        chainId: chainId.toBigInt());
-
-    final signature = starknet_sign(
-        privateKey: privateKey.toBigInt(),
-        messageHash: transactionHash,
-        seed: BigInt.from(32));
-
-    return [Felt(signature.r), Felt(signature.s)];
-  }
-
-  List<Felt> signTransactionsV1({
-    required List<FunctionCall> transactions,
-    required Felt senderAddress,
-    required Felt chainId,
-    Felt? nonce,
-    Felt? maxFee,
-  }) {
-    nonce = nonce ?? Felt.fromInt(0);
-    maxFee = maxFee ?? defaultMaxFee;
-    final calldata = functionCallsToCalldata(functionCalls: transactions);
-
-    final transactionHash = calculateTransactionHashCommon(
-        txHashPrefix: TransactionHashPrefix.invoke.toBigInt(),
-        contractAddress: senderAddress.toBigInt(),
-        version: 1,
-        entryPointSelector: BigInt.parse("0"),
-        calldata: toBigIntList(calldata),
-        maxFee: maxFee.toBigInt(),
-        chainId: chainId.toBigInt(),
-        additionalData: [nonce.toBigInt()]);
+      txHashPrefix: TransactionHashPrefix.invoke.toBigInt(),
+      contractAddress: contractAddress.toBigInt(),
+      version: version,
+      entryPointSelector: version == 0
+          ? getSelectorByName(entryPointSelectorName).toBigInt()
+          : BigInt.parse("0"),
+      calldata: toBigIntList(calldata),
+      maxFee: maxFee.toBigInt(),
+      chainId: chainId.toBigInt(),
+      additionalData: version == 0 ? [] : [nonce.toBigInt()],
+    );
 
     final signature = starknet_sign(
         privateKey: privateKey.toBigInt(),
