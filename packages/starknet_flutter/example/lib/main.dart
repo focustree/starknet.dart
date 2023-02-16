b69d014dab26b1d9a49b384a3ec7d5ee64bad781
Dimitri Dessus <iphoneretro@icloud.com>
2023-02-16 10:39:04 +0100

    ✨ Build Flutter sample Demo

--- packages/starknet_flutter/example/lib/main.dart
+++ packages/starknet_flutter/example/lib/main.dart
@@ -1,21 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:starknet_flutter/starknet_flutter.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const StarknetDemoApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class StarknetDemoApp extends StatelessWidget {
+  const StarknetDemoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "StarkNet Wallet in Flutter",
+    return MaterialApp(
+      title: 'SnackBar Demo',
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
+        appBar: AppBar(
+          title: const Text('Starknet Flutter Demo'),
         ),
+        body: const StarknetDemo(),
       ),
+    );
+  }
+}
+
+class StarknetDemo extends StatefulWidget {
+  const StarknetDemo({super.key});
+
+  @override
+  State<StarknetDemo> createState() => _StarknetDemoState();
+}
+
+class _StarknetDemoState extends State<StarknetDemo> {
+  final _plugin = StarknetFlutter();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextButton(
+          child: const Text('🔐 Store private key'),
+          onPressed: () async {
+            const privateKeyToStore = "My Private Key";
+            await _plugin.storePrivateKey(privateKeyToStore);
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Private key stored: ($privateKeyToStore)'),
+              ),
+            );
+          },
+        ),
+        TextButton(
+          child: const Text('🔑 Get private key'),
+          onPressed: () async {
+            final privateKey = await _plugin.getPrivateKey();
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Private key retrieved: ($privateKey)'),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
