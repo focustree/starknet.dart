acd4fc54a23a9b7a3e7ed24fcc2f573019feed98
Dimitri Dessus <iphoneretro@icloud.com>
2023-02-16 10:37:48 +0100

    ✨ Implement Starknet Flutter plugin

--- packages/starknet_flutter/lib/starknet_flutter.dart
+++ packages/starknet_flutter/lib/starknet_flutter.dart
@@ -1,2 +1,11 @@
+import 'package:starknet_flutter/pigeon.dart';
+
 class StarknetFlutter {
+  Future<String> getPrivateKey() {
+    return StarknetInterface().getPrivateKey();
+  }
+  
+  Future<void> storePrivateKey(String privateKey) {
+    return StarknetInterface().storePrivateKey(privateKey);
+  }
 }
