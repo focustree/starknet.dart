9ebe4ae9223b222383d5352cb27e35944f3211e4
Sébastien Bel <kobatsu.mail@gmail.com>
2023-02-20 16:17:00 +0100

    Fix example, update doc

--- packages/starknet_flutter/example/lib/main.dart
+++ packages/starknet_flutter/example/lib/main.dart
@@ -2,21 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:starknet_flutter_example/store_example.dart';
 
 void main() {
-  runApp(const StarknetDemoApp());
+  runApp(const MainApp());
 }
 
-class StarknetDemoApp extends StatelessWidget {
-  const StarknetDemoApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SnackBar Demo',
+    return const MaterialApp(
+      title: "StarkNet Wallet in Flutter",
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Starknet Flutter Demo'),
-        ),
-        body: const StoreExample(),
+        body: StoreExample(),
       ),
     );
   }
