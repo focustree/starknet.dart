ea8be82982d50c06d448edcf00852d096a7778a4
Sébastien Bel <kobatsu.mail@gmail.com>
2023-02-20 10:59:17 +0100

    Sha256 password hash, add more tests

--- packages/starknet_flutter/lib/src/store/crypto_helper.dart
+++ packages/starknet_flutter/lib/src/store/crypto_helper.dart
@@ -2,62 +2,70 @@ import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:crypto/crypto.dart';
 import 'package:pointycastle/export.dart';
 
 class CryptoHelper {
+  /// Length of the initialization vector used by AES 256 GCM.
   static const ivLength = 12;
 
-  /// [password] must be 16 bytes long, [iv] must be 12 bytes long.
-  String encode({
+  /// Encrypt [plainText] with AES 256 GCM using [password] and [iv].
+  /// The returned String is the base64 encoding of the IV and the cipher text.
+  /// If [iv] is not provided, a random IV will be generated using [getIV].
+  String encrypt({
     required String password,
-    required Uint8List iv,
+    Uint8List? iv,
     required String plainText,
   }) {
-    // The key should be 256 bits (16 bytes) long.
-    Uint8List key = Uint8List.fromList(utf8.encode(password));
-
-    Uint8List plainTextBytes = Uint8List.fromList(utf8.encode(plainText));
-
-    // AES works on blocks of bytes. The padding is used to always have the
-    // same number of bytes in each block.
-    // See https://www.ibm.com/docs/en/zos/2.4.0?topic=rules-pkcs-padding-method
-    PaddedBlockCipher cipher = PaddedBlockCipherImpl(
-      // PKCS7Padding is the most common padding scheme.
-      PKCS7Padding(),
-      GCMBlockCipher(AESEngine()),
+    final usedIV = iv ?? getIV();
+    final cipherText = _cipherAes(
+      key: _hashSha256(password),
+      iv: usedIV,
+      toProcess: Uint8List.fromList(utf8.encode(plainText)),
+      encrypt: true,
     );
-
-    cipher.init(
-      true,
-      PaddedBlockCipherParameters<CipherParameters, CipherParameters>(
-        ParametersWithIV<KeyParameter>(KeyParameter(key), iv),
-        null,
-      ),
-    );
-    Uint8List cipherText = cipher.process(plainTextBytes);
     return base64Encode(
       Uint8List.fromList([
         // Concatenate IV and cipher text to be able to decrypt it later
-        ...iv.toList(),
+        ...usedIV.toList(),
         ...cipherText.toList(),
       ]),
     );
   }
 
-  /// [password] must be 16 bytes long.
-  String decode({
+  /// Decrypt [cipherText] with AES 256 GCM using [password]. The IV will be
+  /// read from the beginning of [cipherText].
+  /// The cipherText is expected to be base64 encoded.
+  String decrypt({
     required String password,
     required String cipherText,
   }) {
-    // The key should be 256 bits (16 bytes) long.
-    Uint8List key = Uint8List.fromList(utf8.encode(password));
-
+    // TODO Handle decode failures (wrong password...)
+    // cipherText is encoded in base64 so decode it first
     Uint8List decodedBytes = base64Decode(cipherText);
 
-    // Split iv and cipher bytes
+    // Split IV and cipher bytes
     final iv = decodedBytes.sublist(0, ivLength);
     final cipherBytes = decodedBytes.sublist(ivLength);
 
+    final plainText = _cipherAes(
+      key: _hashSha256(password),
+      iv: iv,
+      toProcess: cipherBytes,
+      encrypt: false,
+    );
+    return utf8.decode(plainText);
+  }
+
+  /// Process [toProcess] with AES 256 GCM using [key] and [iv].
+  /// When [encrypt] is true, [toProcess] is encrypted, otherwise it is
+  /// decrypted.
+  Uint8List _cipherAes({
+    required Uint8List key,
+    required Uint8List iv,
+    required Uint8List toProcess,
+    required bool encrypt,
+  }) {
     // AES works on blocks of bytes. The padding is used to always have the
     // same number of bytes in each block.
     // See https://www.ibm.com/docs/en/zos/2.4.0?topic=rules-pkcs-padding-method
@@ -68,22 +76,29 @@ class CryptoHelper {
     );
 
     cipher.init(
-      false,
+      // true to encrypt, false to decrypt
+      encrypt,
       PaddedBlockCipherParameters<CipherParameters, CipherParameters>(
         ParametersWithIV<KeyParameter>(KeyParameter(key), iv),
         null,
       ),
     );
-    // TODO Handle decode failures (wrong password...)
-    Uint8List plainText = cipher.process(cipherBytes);
-    return utf8.decode(plainText);
+    return cipher.process(toProcess);
   }
 
+  /// Hash [text] with SHA256.
+  Uint8List _hashSha256(String text) {
+    return Uint8List.fromList(
+      sha256.convert(utf8.encode(text)).bytes,
+    );
+  }
+
+  /// Generates a random IV (Initialization Vector) for AES 256 GCM.
   /// Initialization Vector: 16 bytes to make each encryption unique.
   /// IV will be included at the beginning of the encrypted message to be able
   /// to decrypt it.
-  Uint8List getNonce() {
-    // TODO Generate a better nonce
+  Uint8List getIV() {
+    // TODO Generate a better IV
     // In the secure_random package, the random generation is multiplied many times
     // https://github.com/mingchen/secure_random/blob/master/lib/secure_random.dart
     // It *should* improve randomness, but it is still not 100% bullet proof
@@ -92,7 +107,7 @@ class CryptoHelper {
     // https://pub.dev/packages/xrandom
     final rand = Random();
     return Uint8List.fromList(
-      [for (int i = 0; i < ivLength; i++) rand.nextInt(16)],
+      [for (int i = 0; i < ivLength; i++) rand.nextInt(256)],
     );
   }
 }
