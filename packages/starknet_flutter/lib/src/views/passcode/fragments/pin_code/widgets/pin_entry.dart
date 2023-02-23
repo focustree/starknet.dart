3063b57e880f107c8f92a17476f08c76c3e2cb6b
Dimitri Dessus <iphoneretro@icloud.com>
2023-02-23 16:11:41 +0100

    ✨ Add pin entry widget

---
+++ packages/starknet_flutter/lib/src/views/passcode/fragments/pin_code/widgets/pin_entry.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class PinEntryWidget extends StatelessWidget {
+  final List<int> pin;
+  final int size;
+  const PinEntryWidget({
+    Key? key,
+    required this.pin,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const pinPromptSize = 12.0;
+
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        bool isFilled = index < pin.length;
+
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          width: pinPromptSize,
+          height: pinPromptSize,
+          decoration: BoxDecoration(
+            color: isFilled ? Colors.black : Colors.transparent,
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.black38,
+              width: 1,
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(width: 16);
+      },
+      itemCount: size,
+    );
+  }
+}
