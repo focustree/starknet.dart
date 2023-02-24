3a74838b5dccc10d69319a88a158e283577eff5b
Patrice Tisserand <p.tisserand@gmail.com>
2023-02-24 11:15:54 +0100

    starknet: #120: add support to Braavos account recovery from seed phrase
    Add dependency to bip"é and bip39 packages.
    Please note that Braavos contract classhash, implementation and initializer are hardcoded for demo purpose.

--- packages/starknet/lib/src/account.dart
+++ packages/starknet/lib/src/account.dart
@@ -1,3 +1,8 @@
+import 'dart:typed_data';
+
+import 'package:bip32/bip32.dart' as bip32;
+import 'package:bip39/bip39.dart' as bip39;
+import 'package:pointycastle/digests/sha256.dart';
 import 'package:starknet/src/presets/udc.g.dart';
 import 'package:starknet/starknet.dart';
 
@@ -161,6 +166,16 @@ class Account {
     return txHash;
   }
 
+  Future<bool> get isValid async {
+    final accountClassHash = (await provider.getClassHashAt(
+            contractAddress: accountAddress, blockId: BlockId.latest))
+        .when(
+      result: (result) => result,
+      error: ((error) => Felt.fromInt(0)),
+    );
+    return accountClassHash != Felt.fromInt(0);
+  }
+
   static Future<DeployAccountTransactionResponse> deployAccount({
     required Signer signer,
     required Provider provider,
@@ -197,6 +212,31 @@ class Account {
       constructorCalldata: constructorCalldata,
     )));
   }
+
+  factory Account.fromMnemonic({
+    required List<String> mnemonic,
+    required Provider provider,
+    required Felt chainId,
+    int index = 0,
+    AccountDerivation? accountDerivation,
+  }) {
+    accountDerivation = accountDerivation ??
+        BraavosAccountDerivation(
+          provider: provider,
+          chainId: chainId,
+        );
+    final signer =
+        accountDerivation.deriveSigner(mnemonic: mnemonic, index: index);
+
+    final accountAddress =
+        accountDerivation.computeAddress(publicKey: signer.publicKey);
+    return Account(
+      accountAddress: accountAddress,
+      provider: provider,
+      signer: signer,
+      chainId: chainId,
+    );
+  }
 }
 
 Account getAccount({
@@ -227,4 +267,78 @@ Felt? getDeployedContractAddress(GetTransactionReceipt txReceipt) {
         return contractAddress;
       },
       error: (e) => throw Exception(e.message));
+}
+
+abstract class AccountDerivation {
+  Signer deriveSigner({required List<String> mnemonic, int index = 0});
+  Felt computeAddress({required Felt publicKey});
+
+  Uint8List grindKey(Uint8List key) {
+    final BigInt keyValLimit = pedersenParams.ecOrder;
+    final BigInt sha256MaxDigest = BigInt.parse(
+        '10000000000000000000000000000000000000000000000000000000000000000',
+        radix: 16);
+
+    final maxAllowed = sha256MaxDigest - (sha256MaxDigest % keyValLimit);
+    int index = 0;
+    key = _hashKeyWithIndex(key, index);
+    index++;
+    while (bytesToBigInt(key) > maxAllowed) {
+      key = _hashKeyWithIndex(key, index);
+      index++;
+    }
+    final result = bytesToBigInt(key) % keyValLimit;
+    return result.toUint8List();
+  }
+
+  Uint8List _hashKeyWithIndex(Uint8List key, int index) {
+    // Uint8List are not growable
+    final data = Uint8List.fromList(key + [index]);
+    return SHA256Digest().process(data);
+  }
+}
+
+class BraavosAccountDerivation extends AccountDerivation {
+  final Provider provider;
+  final Felt chainId;
+  final String pathPrefix = "m/44'/9004'/0'/0";
+
+  // FIXME: hardcoded value for testnet 2023-02-24
+  final classHash = Felt.fromHexString(
+      "0x03131fa018d520a037686ce3efddeab8f28895662f019ca3ca18a626650f7d1e");
+  final implementationAddress = Felt.fromHexString(
+      "0x5aa23d5bb71ddaa783da7ea79d405315bafa7cf0387a74f4593578c3e9e6570");
+  final initializerSelector = Felt.fromHexString(
+      "0x2dd76e7ad84dbed81c314ffe5e7a7cacfb8f4836f01af4e913f275f89a3de1a");
+
+  BraavosAccountDerivation({
+    required this.provider,
+    required this.chainId,
+  });
+
+  @override
+  Signer deriveSigner({required List<String> mnemonic, int index = 0}) {
+    final seed = bip39.mnemonicToSeed(mnemonic.join(" "));
+    final nodeFromSeed = bip32.BIP32.fromSeed(seed);
+    final child = nodeFromSeed.derivePath('$pathPrefix/$index');
+    Uint8List key = child.privateKey!;
+    key = grindKey(key);
+    final privateKey = Felt(bytesToBigInt(key));
+    return Signer(privateKey: privateKey);
+  }
+
+  @override
+  Felt computeAddress({required Felt publicKey}) {
+    final initializeData = [publicKey];
+    final calldata = [
+      implementationAddress,
+      initializerSelector,
+      Felt.fromInt(initializeData.length),
+      ...initializeData,
+    ];
+    final salt = publicKey;
+    final accountAddress = Contract.computeAddress(
+        classHash: classHash, calldata: calldata, salt: salt);
+    return accountAddress;
+  }
 }
