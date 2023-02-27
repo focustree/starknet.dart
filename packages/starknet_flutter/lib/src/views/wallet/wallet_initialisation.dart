2e46be5d4e6e50ececa78227b976a5bd92c0300f
Dimitri Dessus <iphoneretro@icloud.com>
2023-02-27 14:47:51 +0100

    🚚 Split router files

--- packages/starknet_flutter/lib/src/views/wallet/wallet_initialisation.dart
+++ packages/starknet_flutter/lib/src/views/wallet/wallet_initialisation.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:starknet_flutter/src/views/wallet/routes/create_wallet/choose_network_screen.dart';
-import 'package:starknet_flutter/src/views/wallet/routes/create_wallet/create_seed_screen.dart';
-import 'package:starknet_flutter/src/views/wallet/routes/restore_wallet/protect_wallet_screen.dart';
-import 'package:starknet_flutter/src/views/wallet/routes/restore_wallet/restore_wallet_screen.dart';
 import 'package:starknet_flutter/src/views/wallet/wallet_initialisation_observer.dart';
 import 'package:starknet_flutter/src/views/wallet/routes/welcome/wallet_welcome_view.dart';
+import 'package:starknet_flutter/src/views/wallet/wallet_initialisation_router.dart';
 import 'package:starknet_flutter/src/views/widgets/bouncing_button.dart';
 
 import 'wallet_initialisation_presenter.dart';
@@ -101,55 +98,12 @@ class _WalletInitialisationPageState extends State<WalletInitialisationPage>
       body: Navigator(
         key: _navigatorKey,
         observers: [observer],
-        onGenerateRoute: (settings) {
-          switch (settings.name) {
-            case WalletWelcomeView.routeName:
-              return MaterialPageRoute(
-                settings: settings,
-                builder: (context) => WalletWelcomeView(
-                  model: model,
-                  presenter: presenter,
-                ),
-              );
-            case CreateSeedScreen.routeName:
-              return MaterialPageRoute(
-                settings: settings,
-                builder: (context) => CreateSeedScreen(
-                  model: model,
-                  presenter: presenter,
-                ),
-              );
-            case ChooseNetworkScreen.routeName:
-              return MaterialPageRoute(
-                settings: settings,
-                builder: (context) => ChooseNetworkScreen(
-                  model: model,
-                  presenter: presenter,
-                ),
-              );
-            case ProtectWalletScreen.routeName:
-              return MaterialPageRoute(
-                settings: settings,
-                builder: (context) => ProtectWalletScreen(
-                  model: model,
-                  presenter: presenter,
-                ),
-              );
-            case RestoreWalletScreen.routeName:
-              return MaterialPageRoute(
-                settings: settings,
-                builder: (context) => RestoreWalletScreen(
-                  model: model,
-                  presenter: presenter,
-                ),
-              );
-            default:
-              return MaterialPageRoute(
-                settings: settings,
-                builder: (context) => const SizedBox.shrink(),
-              );
-          }
-        },
+        onGenerateRoute: (settings) =>
+            WalletInitialisationRouter.onGenerateRoute(
+          settings,
+          model,
+          presenter,
+        ),
       ),
     );
   }
