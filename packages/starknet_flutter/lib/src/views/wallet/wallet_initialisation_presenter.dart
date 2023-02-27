557d98fc7e0b234d17ce459e410c9e2e005d1b44
Dimitri Dessus <iphoneretro@icloud.com>
2023-02-27 16:32:36 +0100

    ✨ Add title in app bar

--- packages/starknet_flutter/lib/src/views/wallet/wallet_initialisation_presenter.dart
+++ packages/starknet_flutter/lib/src/views/wallet/wallet_initialisation_presenter.dart
@@ -1,3 +1,11 @@
+import 'dart:async';
+
+import 'package:starknet_flutter/src/views/wallet/routes/create_wallet/choose_network_screen.dart';
+import 'package:starknet_flutter/src/views/wallet/routes/create_wallet/create_seed_screen.dart';
+import 'package:starknet_flutter/src/views/wallet/routes/restore_wallet/protect_wallet_screen.dart';
+import 'package:starknet_flutter/src/views/wallet/routes/restore_wallet/restore_wallet_screen.dart';
+import 'package:starknet_flutter/src/views/wallet/routes/welcome/wallet_welcome_view.dart';
+
 import 'wallet_initialisation.dart';
 import 'wallet_initialisation_viewmodel.dart';
 
@@ -5,12 +13,18 @@ class WalletInitialisationPresenter {
   final WalletInitialisationViewModel viewModel;
   final WalletInitialisationView viewInterface;
 
+  StreamSubscription<String?>? _subscription;
+
   WalletInitialisationPresenter(
     this.viewModel,
     this.viewInterface,
   );
 
   WalletInitialisationPresenter init() {
+    _subscription = viewModel.didChange.stream.listen((routeName) {
+      viewModel.routeName = routeName;
+      viewInterface.refresh();
+    });
     return this;
   }
 
@@ -20,7 +34,9 @@ class WalletInitialisationPresenter {
         );
   }
 
-  void dispose() {}
+  void dispose() {
+    _subscription?.cancel();
+  }
 
   void createWallet() {
     viewInterface.closeModal();
