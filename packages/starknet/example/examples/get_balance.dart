d32ffd87ca07f3be5c659e7d80bcc4d83e5e9276
Gabin Marignier <gabin.contact@gmail.com>
2023-03-01 12:18:24 +0100

    Use test account

--- packages/starknet/example/examples/get_balance.dart
+++ packages/starknet/example/examples/get_balance.dart
@@ -1,12 +1,13 @@
 import 'package:starknet/starknet.dart';
 
+final ethContractAddress = Felt.fromHexString(
+    '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7');
+final ethDecimals = 18;
+
 void main() async {
   final provider = JsonRpcProvider.infuraGoerliTestnet;
   final accountAddress = Felt.fromHexString(
-      '0x046a1aa85bb0e68cd29fadbc81791208ddebee17886f075935e5b72f4aa898aa');
-  final ethContractAddress = Felt.fromHexString(
-      '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7');
-  final ethDecimals = 18;
+      '0x7e6c6c00ceaf1286808de55ab217a3839f1e59d988361f529ed8d0f9dfdaa37');
 
   final response = await provider.call(
     request: FunctionCall(
@@ -24,7 +25,7 @@ void main() async {
     result: (result) {
       final ethBalance = Uint256.fromFeltList(result).toBigInt() /
           BigInt.from(10).pow(ethDecimals);
-      print("ETH balance: $ethBalance"); // ETH balance: 8.142616847371661
+      print("ETH balance: $ethBalance"); // ETH balance: 0.006397677730418121
     },
   );
 }
