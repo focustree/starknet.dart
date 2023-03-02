d486b92728788386652c851171fceeb18167156d
Dimitri Dessus <iphoneretro@icloud.com>
2023-03-02 11:32:53 +0100

    🗑️ Remove unused container color

--- packages/starknet_flutter/lib/src/views/wallet/routes/restore_wallet/protect_wallet_screen.dart
+++ packages/starknet_flutter/lib/src/views/wallet/routes/restore_wallet/protect_wallet_screen.dart
@@ -19,31 +19,29 @@ class ProtectWalletScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const Text(
-              "This extra layer of security helps prevent someone with your phone from accessing your funds.",
-            ),
-            const SizedBox(height: 32),
-            FutureBuilder(
-              future: StarknetStore.secure(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return FutureBuilder(
-                    future: snapshot.requireData.when(
-                      biometric: (biometric) {
-                        return StarknetButton.plain(
-                          icon: const Icon(
-                            Icons.fingerprint,
-                            color: Colors.white,
-                          ),
-                          text: 'Protect my wallet with biometric',
-                          onTap: () async {
-                            // Create wallet and account
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          const Text(
+            "This extra layer of security helps prevent someone with your phone from accessing your funds.",
+          ),
+          const SizedBox(height: 32),
+          FutureBuilder(
+            future: StarknetStore.secure(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return FutureBuilder(
+                  future: snapshot.requireData.when(
+                    biometric: (biometric) {
+                      return StarknetButton.plain(
+                        icon: const Icon(
+                          Icons.fingerprint,
+                          color: Colors.white,
+                        ),
+                        text: 'Protect my wallet with biometric',
+                        onTap: () async {
+                          // Create wallet and account
                             final wallet = Wallet(
                               name: "Wallet 1",
                               order: 0,
@@ -63,56 +61,55 @@ class ProtectWalletScreen extends StatelessWidget {
                             await publicStore.storeWallet(wallet);
 
                             // Store seed phrase and private key securely
-                            await biometric.storeSeedPhrase(
-                              id: wallet.walletId,
-                              seedPhrase: model.seedPhrase!,
-                            );
-                            await biometric.storePrivateKey(
+                          await biometric.storeSeedPhrase(
+                            id: wallet.walletId,
+                            seedPhrase: model.seedPhrase!,
+                          );
+                          await biometric.storePrivateKey(
                               id: account.privateKeyId,
                               privateKey: model.account!.signer.privateKey
                                   .toBigInt()
                                   .toUint8List(),
                             );
 
-                            presenter.onWalletRestored(wallet);
-                          },
-                        );
-                      },
-                      password: (password) {
-                        return StarknetButton.plain(
-                          icon: const Icon(
-                            Icons.key,
-                            color: Colors.white,
-                          ),
-                          text: 'Protect my wallet with a password',
-                          onTap: () async {
-                            // Store seed phrase and private key
-                            // TODO Prompt user for password
-                            await password.storeSeedPhrase(
-                              id: "uuid1",
-                              seedPhrase: model.seedPhrase!,
-                              password: "password",
-                            );
-                            // TODO Store private key
-
-                            // Store uuids into hive or similar
-
-                            // TODO Navigate to home screen
-                          },
-                        );
-                      },
-                    ),
-                    builder: (context, snapshotWhen) {
-                      return snapshotWhen.data ?? const SizedBox.shrink();
+                          presenter.onWalletRestored(wallet);
+                        },
+                      );
                     },
-                  );
-                } else {
-                  return const SizedBox.shrink();
-                }
-              },
-            ),
-          ],
-        ),
+                    password: (password) {
+                      return StarknetButton.plain(
+                        icon: const Icon(
+                          Icons.key,
+                          color: Colors.white,
+                        ),
+                        text: 'Protect my wallet with a password',
+                        onTap: () async {
+                          // Store seed phrase and private key
+                          // TODO Prompt user for password
+                          await password.storeSeedPhrase(
+                            id: "uuid1",
+                            seedPhrase: model.seedPhrase!,
+                            password: "password",
+                          );
+                          // TODO Store private key
+
+                          // Store uuids into hive or similar
+
+                          // TODO Navigate to home screen
+                        },
+                      );
+                    },
+                  ),
+                  builder: (context, snapshotWhen) {
+                    return snapshotWhen.data ?? const SizedBox.shrink();
+                  },
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
+            },
+          ),
+        ],
       ),
     );
   }
--- packages/starknet_flutter/lib/src/views/wallet/routes/restore_wallet/restore_wallet_screen.dart
+++ packages/starknet_flutter/lib/src/views/wallet/routes/restore_wallet/restore_wallet_screen.dart
@@ -35,101 +35,99 @@ class _RestoreWalletScreenState extends State<RestoreWalletScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "This is a 12-word phrase you were given when you created your previous wallet.",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.labelMedium,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Enter your recovery phrase",
-                // labelText: "Your recovery phrase",
-                border: const OutlineInputBorder(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "This is a 12-word phrase you were given when you created your previous wallet.",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter your recovery phrase",
+              // labelText: "Your recovery phrase",
+              border: const OutlineInputBorder(),
                 counterText: "$_nbWordsInSeedPhrase/12",
                 counterStyle: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: _nbWordsInSeedPhrase == 12 ? Colors.green : Colors.red,
                 ),
-              ),
-              maxLines: 5,
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-              ),
-              onChanged: (value) {
-                setState(() {
-                  _seedPhrase = value;
-                });
-              },
             ),
-            const SizedBox(height: 32),
-            Text(
-              "Select your account type",
-              style: Theme.of(context).textTheme.bodyMedium,
+            maxLines: 5,
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
             ),
-            const SizedBox(height: 15),
-            Expanded(
-              child: ListView.separated(
-                itemCount: StarknetAccountType.values.length,
-                separatorBuilder: (context, i) => const SizedBox(height: 10),
-                itemBuilder: (context, i) {
-                  final accountType = StarknetAccountType.values[i];
+            onChanged: (value) {
+              setState(() {
+                _seedPhrase = value;
+              });
+            },
+          ),
+          const SizedBox(height: 32),
+          Text(
+            "Select your account type",
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(height: 15),
+          Expanded(
+            child: ListView.separated(
+              itemCount: StarknetAccountType.values.length,
+              separatorBuilder: (context, i) => const SizedBox(height: 10),
+              itemBuilder: (context, i) {
+                final accountType = StarknetAccountType.values[i];
 
-                  return BouncingWidget(
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 200),
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: _accountType == accountType
-                            ? Theme.of(context).primaryColor
-                            : Theme.of(context).primaryColor.withOpacity(0.08),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          children: [
-                            SvgPicture.asset(
-                              "packages/starknet_flutter/assets/images/wallets/${accountType.logoAssetPath}",
-                              width: 20,
-                              height: 20,
+                return BouncingWidget(
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 200),
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: _accountType == accountType
+                          ? Theme.of(context).primaryColor
+                          : Theme.of(context).primaryColor.withOpacity(0.08),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            "packages/starknet_flutter/assets/images/wallets/${accountType.logoAssetPath}",
+                            width: 20,
+                            height: 20,
+                          ),
+                          const SizedBox(width: 15),
+                          Text(
+                            accountType.title,
+                            style: TextStyle(
+                              color: _accountType == accountType
+                                  ? Theme.of(context).colorScheme.onPrimary
+                                  : Colors.black87,
                             ),
-                            const SizedBox(width: 15),
-                            Text(
-                              accountType.title,
-                              style: TextStyle(
-                                color: _accountType == accountType
-                                    ? Theme.of(context).colorScheme.onPrimary
-                                    : Colors.black87,
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                    onTap: () {
-                      setState(() {
-                        _accountType = StarknetAccountType.values[i];
-                      });
-                    },
-                  );
-                },
-              ),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _accountType = StarknetAccountType.values[i];
+                    });
+                  },
+                );
+              },
             ),
-            const SizedBox(height: 15),
-            StarknetButton.plain(
-              onTap: _seedPhrase?.trim().isNotEmpty == true &&
-                      _seedPhrase!.trim().split(" ").length == 12
-                  ? () async {
-                      // Test if seedPhrase with selected accountType resolve to a smart contract address
-                      final seedPhrase = _seedPhrase!.trim().split(" ");
+          ),
+          const SizedBox(height: 15),
+          StarknetButton.plain(
+            onTap:
+                    _seedPhrase?.trim().isNotEmpty == true &&
+                    _seedPhrase!.trim().split(" ").length == 12
+                ? () async {// Test if seedPhrase with selected accountType resolve to a smart contract address
+                    final seedPhrase = _seedPhrase!.trim().split(" ");
 
                       final provider =
                           JsonRpcProvider(nodeUri: infuraGoerliTestnetUri);
