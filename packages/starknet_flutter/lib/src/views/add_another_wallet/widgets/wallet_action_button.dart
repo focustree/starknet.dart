0b421ae30f1a2c52f30c66e59ff2286d316ad419
Dimitri Dessus <iphoneretro@icloud.com>
2023-03-02 11:33:13 +0100

    🐛 Fix maximum action button size

--- packages/starknet_flutter/lib/src/views/add_another_wallet/widgets/wallet_action_button.dart
+++ packages/starknet_flutter/lib/src/views/add_another_wallet/widgets/wallet_action_button.dart
@@ -21,66 +21,71 @@ class WalletActionButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return BouncingWidget(
       onTap: onTap,
-      child: Container(
-        width: double.infinity,
-        height: 150,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: gradient,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+            maxWidth: 600,
           ),
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: gradient.first.withOpacity(0.4),
-              blurRadius: 15,
-              offset: const Offset(0, 5),
+        child: Container(
+          width: double.infinity,
+          height: 150,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: gradient,
             ),
-          ],
-        ),
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 30, right: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    title,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 19,
-                    ),
-                  ),
-                  const SizedBox(height: 7),
-                  Text(
-                    subtitle,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 13,
-                    ),
-                  ),
-                ],
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: gradient.first.withOpacity(0.4),
+                blurRadius: 15,
+                offset: const Offset(0, 5),
               ),
-            ),
-            Positioned(
-              right: -20,
-              bottom: -40,
-              child: Opacity(
-                opacity: 0.3,
-                child: Icon(
-                  icon,
-                  color: Colors.white,
-                  size: 160,
+            ],
+          ),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      title,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 19,
+                      ),
+                    ),
+                    const SizedBox(height: 7),
+                    Text(
+                      subtitle,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              Positioned(
+                right: -20,
+                bottom: -40,
+                child: Opacity(
+                  opacity: 0.3,
+                  child: Icon(
+                    icon,
+                    color: Colors.white,
+                    size: 160,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
