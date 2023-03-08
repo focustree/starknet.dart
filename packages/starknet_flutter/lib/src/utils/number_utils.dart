6fd4048c4d66b2aefc04f2e19c1727c2b9f97c73
Dimitri Dessus <iphoneretro@icloud.com>
2023-03-08 10:22:55 +0100

    🐛 Fix truncate balance

--- packages/starknet_flutter/lib/src/utils/number_utils.dart
+++ packages/starknet_flutter/lib/src/utils/number_utils.dart
@@ -3,9 +3,20 @@ extension StarknetNumberUtils on num {
   ///
   /// ex: 0.5456945765.truncateToDouble(4) = 0.5456
   double truncateBalance({int precision = 4}) {
-    if (this == 0) return 0.0;
+    if (this == 0) {
+      return 0.0;
+    }
 
     String numToString = toString();
+    if (!numToString.contains('.')) {
+      return double.parse(numToString);
+    }
+    
+    final digits = numToString.split('.')[1].length;
+    if (digits <= precision) {
+      return double.parse(numToString);
+    }
+
     String numTruncatedString =
         numToString.substring(0, numToString.indexOf('.') + precision + 1);
     return double.parse(numTruncatedString);
