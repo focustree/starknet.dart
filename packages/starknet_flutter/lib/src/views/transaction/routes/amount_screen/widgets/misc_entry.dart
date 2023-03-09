93cbd332915db8abce33eb1932944496225f0b8d
Dimitri Dessus <iphoneretro@icloud.com>
2023-03-09 11:42:34 +0100

    🐛 Add void callback type

--- packages/starknet_flutter/lib/src/views/transaction/routes/amount_screen/widgets/misc_entry.dart
+++ packages/starknet_flutter/lib/src/views/transaction/routes/amount_screen/widgets/misc_entry.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:starknet_flutter/src/views/widgets/bouncing_widget.dart';
 
 class MiscEntryWidget extends StatelessWidget {
-  final Function()? onTap;
+  final VoidCallback? onTap;
   final Widget child;
   const MiscEntryWidget({
     Key? key,
