49fba32ef863105466690fe39176eb3d46e3eba0
Dimitri Dessus <iphoneretro@icloud.com>
2023-03-09 16:39:12 +0100

    🛂 Add camera permission for iOS & macOS

--- packages/starknet_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/starknet_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import emoji_picker_flutter
+import mobile_scanner
 import path_provider_foundation
 import shared_preferences_foundation
 import starknet_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   StarknetFlutterPlugin.register(with: registry.registrar(forPlugin: "StarknetFlutterPlugin"))
