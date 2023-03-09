605cf9f699a7de8a6415bd739c42dfee6c1619ab
Dimitri Dessus <iphoneretro@icloud.com>
2023-03-09 16:40:49 +0100

    ✨ Add a way to generate code in string extension

--- packages/starknet_flutter/lib/src/utils/string_utils.dart
+++ packages/starknet_flutter/lib/src/utils/string_utils.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
 extension StarknetStringUtils on String {
   String truncateMiddle({int truncateLength = 17}) {
     const kDotLength = 3;
@@ -9,5 +12,20 @@ extension StarknetStringUtils on String {
     int textLength = (truncateLength / 2).floor() - kDotLength;
     int placeholderLength = truncateLength - textLength - kDotLength;
     return "${substring(0, placeholderLength)}...${substring(length - textLength)}";
+  }
+
+  // TODO: add some config parameters
+  /// generate a QR code
+  Widget generateQRCode({
+    int version = QrVersions.auto,
+    double? size,
+  }) {
+    return QrImage(
+      data: this,
+      // version: QrVersions.auto,
+      version: version,
+      size: size,
+      // size: 200.0,
+    );
   }
 }
