3cd141e41e6dd59e7f4f4347e3b39ff2ae1fcf22
Paul Reboullet <paulreboullet@gmail.com>
2023-03-28 13:35:14 +0200

    WIP

--- packages/starknet_flutter/lib/src/services/protect_wallet_service.dart
+++ packages/starknet_flutter/lib/src/services/protect_wallet_service.dart
@@ -163,4 +163,55 @@ abstract class ProtectWalletService {
       }
     }
   }
+
+  static Future<PublicAccount> protectWithPassword(
+    BuildContext context, {
+    required PasswordStore passwordStore,
+    required StarknetAccountType accountType,
+    required Wallet wallet,
+    required Account account,
+    List<String>? seedPhrase,
+    required Future<String?> Function() passwordPrompt,
+    int accountIndex = 0,
+  }) async {
+    final passwordInput = await passwordPrompt();
+    if (passwordInput == null) {
+      // Password == null means that the user cancelled the prompt
+      throw PasswordCancelledException();
+    } else {
+      if (!await passwordStore.isGoodPassword(passwordInput)) {
+        throw WrongPasswordException(passwordInput);
+      } else {
+        final publicAccount = PublicAccount.from(
+          account: account,
+          walletId: wallet.walletId,
+          order: accountIndex,
+        );
+
+        final publicStore = StarknetStore.public();
+        // First store the account in the public store
+        await publicStore.storeAccount(publicAccount);
+        // Now, add this account in the previously created wallet
+        wallet.accounts.add(publicAccount);
+        // Finally, store the wallet (which now contains the account)
+        await publicStore.storeWallet(wallet);
+
+        // Store seed phrase and private key securely
+        if (seedPhrase != null) {
+          await passwordStore.storeSeedPhrase(
+            id: wallet.walletId,
+            seedPhrase: seedPhrase,
+            password: passwordInput,
+          );
+        }
+        await passwordStore.storePrivateKey(
+          id: publicAccount.privateKeyId,
+          privateKey: account.signer.privateKey.toBigInt().toUint8List(),
+          password: passwordInput,
+        );
+
+        return publicAccount;
+      }
+    }
+  }
 }
