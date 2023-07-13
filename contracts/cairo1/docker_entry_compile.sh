48863a7523f89eefe485ec348dd7ee95acd274f5
Patrice Tisserand <p.tisserand@gmail.com>
2023-07-13 12:08:42 +0200

    contracts: add `--add-pythonic-hints` option to `starknet-sierra-compile`

--- contracts/cairo1/docker_entry_compile.sh
+++ contracts/cairo1/docker_entry_compile.sh
@@ -5,7 +5,7 @@ set -e
 
 compile () {
   starknet-compile $1 "${2}_sierra.txt"
-  starknet-sierra-compile "${2}_sierra.txt" "${2}_compiled.txt"
+  starknet-sierra-compile --add-pythonic-hints "${2}_sierra.txt" "${2}_compiled.txt"
   chown $USER_ID:$GROUP_ID "${2}_sierra.txt"
   chown $USER_ID:$GROUP_ID "${2}_compiled.txt"
 }
