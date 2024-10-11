28eaff83b4f575313294ed517d4e9c2ec920d076
devnet0x <devnet0x@gmail.com>
2024-10-11 11:32:06 -0300

    Restore 2 unchanged files.

--- packages/wallet_kit/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/wallet_kit/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import path_provider_foundation
 import secure_store
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SecureStorePlugin.register(with: registry.registrar(forPlugin: "SecureStorePlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
