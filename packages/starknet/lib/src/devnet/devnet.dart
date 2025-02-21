cba2a261068c62eefdb743a7a263966fdf4a6513
rukafe0x <rukafe0x@gmail.com>
2025-02-21 14:12:05 -0300

    Remove comment

--- packages/starknet/lib/src/devnet/devnet.dart
+++ packages/starknet/lib/src/devnet/devnet.dart
@@ -1,7 +1,6 @@
 import 'package:starknet_provider/starknet_provider.dart';
 import 'model/mint_transaction.dart';
 
-// Extension methods for devnet-specific functionality
 class Devnet {
   /// Mints tokens on the devnet
   static Future<MintResponse> mintTransaction(
