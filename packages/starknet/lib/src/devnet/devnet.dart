551a731b6ed3f27604c28c61a09fa5ecda60c64f
rukafe0x <rukafe0x@gmail.com>
2025-04-01 17:27:01 -0300

    Apply aicoderabbit recommendations

--- packages/starknet/lib/src/devnet/devnet.dart
+++ packages/starknet/lib/src/devnet/devnet.dart
@@ -1,6 +1,7 @@
 import 'package:starknet_provider/starknet_provider.dart';
 import 'model/mint_transaction.dart';
 import 'model/predeployed_account.dart';
+
 class Devnet {
   /// Mints tokens on the devnet
   static Future<MintResponse> mintTransaction(
@@ -20,15 +21,19 @@ class Devnet {
   /// Get predeployed accounts from devnet with balances
   static Future<List<PredeployedAccount>> getPredeployedAccounts(
       Uri nodeUri) async {
-    final response = await callRpcEndpoint(
-      nodeUri: nodeUri,
-      method: 'devnet_getPredeployedAccounts',
-      params: {'with_balance': true},
-    );
-    
-    final accountsList = response['result'] as List<dynamic>;
-    return accountsList
-        .map((e) => PredeployedAccount.fromJson(e as Map<String, dynamic>))
-        .toList();
+    try {
+      final response = await callRpcEndpoint(
+        nodeUri: nodeUri,
+        method: 'devnet_getPredeployedAccounts',
+        params: {'with_balance': true},
+      );
+
+      final accountsList = response['result'] as List<dynamic>;
+      return accountsList
+          .map((e) => PredeployedAccount.fromJson(e as Map<String, dynamic>))
+          .toList();
+    } catch (e) {
+      throw ArgumentError('Failed to get predeployed accounts: $e');
+    }
   }
 }
