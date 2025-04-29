904a6f8665f0cf2cc1baf8bf60770f1cc8e4d9d0
redDwarf03 <sylvain@archethic.net>
2025-04-29 15:34:50 +0200

    wallet_kit should check if account is deployed at startup

--- packages/wallet_kit/lib/utils/persisted_notifier_state.dart
+++ packages/wallet_kit/lib/utils/persisted_notifier_state.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:easy_debounce/easy_debounce.dart';
 import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wallet_kit/utils/debug_print.dart';
 
 mixin PersistedState<T extends PersistableState> on AutoDisposeNotifier<T> {
   String get boxName;
@@ -14,13 +15,30 @@ mixin PersistedState<T extends PersistableState> on AutoDisposeNotifier<T> {
     persistState();
   }
 
+  /// Hook called after state is loaded from persistence but before it's set.
+  /// Override this to perform validation or migration logic.
+  /// Return `null` to prevent the loaded state from being applied.
+  Future<T?> onStateLoaded(T loadedState) async {
+    return loadedState;
+  }
+
   loadPersistedState() async {
     final box = await Hive.openBox<String>(boxName);
     final jsonString = box.get(boxName);
     if (jsonString == null) {
-      return null;
+      return;
     }
-    state = fromJson(jsonDecode(jsonString));
+    try {
+      T loadedState = fromJson(jsonDecode(jsonString));
+      T? validatedState = await onStateLoaded(loadedState);
+
+      if (validatedState != null) {
+        super.state = validatedState;
+      }
+    } catch (e, stackTrace) {
+      debugPrint(
+          "Error loading or validating persisted state for $boxName: $e\n$stackTrace");
+    }
   }
 
   persistState() async {
