cd8507b690395ca414857e9d3e6ad1ac05b5290b
imdavyking <yungdynamic53@gmail.com>
2025-05-23 13:16:08 +0100

    chore: added toCalldata to uint256

--- packages/starknet/lib/src/core/types/uint256.dart
+++ packages/starknet/lib/src/core/types/uint256.dart
@@ -70,3 +70,25 @@ class Uint256 {
     );
   }
 }
+
+extension Uint256ListToCalldata on List<Uint256> {
+  List<Felt> toCalldata() {
+    return [
+      Felt.fromInt(length),
+      for (final uint in this) ...[uint.low, uint.high],
+    ];
+  }
+}
+
+extension ListUint256ToCalldata on List<List<Uint256>> {
+  List<Felt> toCalldata() {
+    if (isEmpty) {
+      return [Felt.zero];
+    }
+    final convertedList = map((e) => e.toCalldata()).toList();
+    return [
+      Felt.fromInt(length),
+      ...convertedList.expand((list) => list),
+    ];
+  }
+}
