b5d76912412aca7678af123b14a7948337035df0
ptisserand <p.tisserand@gmail.com>
2025-08-04 18:17:28 +0200

    fix specification url

--- packages/starknet_provider/lib/src/read_provider.dart
+++ packages/starknet_provider/lib/src/read_provider.dart
@@ -128,13 +128,13 @@ abstract class ReadProvider {
 
   /// Estimate the fee for a given message from L1 to L2.
   ///
-  /// [Spec](https://github.com/starkware-libs/starknet-specs/blob/tag/sha1sum/api/starknet_api_openrpc.json)
+  /// [Spec](https://github.com/starkware-libs/starknet-specs/blob/v0.8.1/api/starknet_api_openrpc.json#L673)
   Future<EstimateMessageFee> estimateMessageFee(
       EstimateMessageFeeRequest request);
 
   /// Fetches a block along with its transaction receipts.
   ///
-  /// [Spec](https://github.com/starkware-libs/starknet-specs/blob/tag/sha1sum/api/starknet_api_openrpc.json)
+  /// [Spec](https://github.com/starkware-libs/starknet-specs/blob/v0.8.1/api/starknet_api_openrpc.json#L99)
   Future<BlockWithReceipts> getBlockWithReceipts(BlockId blockId);
 
   /// Returns the version of the Starknet JSON-RPC specification being used
