11410e2e4ba2d3e6cd9a299562bbacb0aac5c9ba
rukafe0x <rukafe0x@gmail.com>
2025-08-05 17:39:12 -0400

    Keep avnu test with hardcoded max gas token amount

--- packages/avnu_provider/test/integration/provider_test.dart
+++ packages/avnu_provider/test/integration/provider_test.dart
@@ -66,28 +66,11 @@ void main() {
           }
         ];
 
-        final functionCalls = calls
-            .map((call) => FunctionCall(
-                  contractAddress:
-                      Felt.fromHexString(call['contractAddress'] as String),
-                  entryPointSelector:
-                      getSelectorByName(call['entrypoint'] as String),
-                  calldata: (call['calldata'] as List<String>)
-                      .map((data) => Felt.fromHexString(data))
-                      .toList(),
-                ))
-            .toList();
-
         final gasTokenAddress =
             '0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d';
+        // Get maxFee as hex string
 
-        final maxGasTokenAmount =
-            (await sepoliaAccount0.getEstimateMaxFeeForInvokeTx(
-          functionCalls: functionCalls,
-          feeMultiplier: 50.0,
-        ))
-                .maxFee
-                .toHexString();
+        final maxGasTokenAmount = "0x23910f84272b14";
 
         //just for testing, we hardcode the account class hash to the ArgentX account class hash
         //in a real scenario, we would get the account class hash from the Starknetprovider
